2fb5dcad4884b09aa3148a178bdf385c2fb6f2b8
jsr88431 <jsr88431@gmail.com>
2018-10-23 09:49:05 -0700

    Updates

---
+++ precache-manifest.85993c19946d0294fc0c22abdba59698.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ffa1f4187ecf54ec1d4",
+    "url": "/clicky-game-react/static/js/runtime~main.1ffa1f41.js"
+  },
+  {
+    "revision": "1e3dd041b6bf429ff455",
+    "url": "/clicky-game-react/static/js/main.1e3dd041.chunk.js"
+  },
+  {
+    "revision": "709840df12e4494d4644",
+    "url": "/clicky-game-react/static/js/1.709840df.chunk.js"
+  },
+  {
+    "revision": "b364b657efd1ff9158c6c80a2939bd42",
+    "url": "/clicky-game-react/index.html"
+  }
+];
